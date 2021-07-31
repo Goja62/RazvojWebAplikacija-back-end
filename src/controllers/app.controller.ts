454c42c9737df89a2b7f9e04d2c2dae105184d20
Goja <gortnar@beograd.com>
2021-07-31 15:17:29 +0200

    DTO i implementacija opacija dodavanja i izmene - bez provere 31.07.2021

---
+++ src/controllers/app.controller.ts
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+import { Controller } from '@nestjs/common';
+import { AdministratorService } from '../services/administrator/administrator.service';
+
+@Controller()
+export class AppController {
+  constructor(private administratorService: AdministratorService) {}
+  
+}
